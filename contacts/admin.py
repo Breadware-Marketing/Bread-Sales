a8acefd403056e902f9a5e546f10b3ebd55fdc26
Eduardo Morales <anisedmo@gmail.com>
2018-03-03 16:10:07 -0800

    Added Model to Admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import LeadSource, Company, Contacts
 # Register your models here.
+
+admin.site.register(LeadSource)
+admin.site.register(Company)
+admin.site.register(Contacts)
